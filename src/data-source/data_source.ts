c5c160c5af17bef56607573bfcff6ad54e58e2ce
phutri27 <triphu27@gmail.com>
2026-02-12 08:42:52 +0000

    Create data source for map and characters

---
+++ src/data-source/data_source.ts
@@ -0,0 +1,18 @@
+export const imgArr:string[] = [
+  "../../../public/map_1.png",
+  "../../../public/map_2.png",
+  "../../../public/map_3.png"
+]
+
+export type CharactersInfo = {
+    imgUrl: string,
+    name: string,
+}
+
+export const characters: CharactersInfo[]  = [
+    {imgUrl: "../../../public/characters/waldo.png", name: "Waldo"},
+    {imgUrl:"../../../public/characters/wenda.png", name: "Wenda"},
+    {imgUrl: "../../../public/characters/odlaw.png", name: "Odlaw"},
+    {imgUrl: "../../../public/characters/wizard.png", name: "Wizard"}
+]
+
